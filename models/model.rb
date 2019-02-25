2e459fd9e17a95b7225f5cb49bff51caff5d9b0e
Arieta Haskaj <arietah8545@hstat.org>
2019-02-25 03:14:27 +0000

    spotify playlist to songs, then get lyrics for all

---
+++ models/model.rb
@@ -0,0 +1,51 @@
+require 'lyricfy'
+require 'rspotify'
+require 'pp'
+
+RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+
+class Song
+    attr_reader :name, :artist, :album_img, :lyrics
+    
+    def initialize(name,artist,album_img)
+        @name = name
+        @artist = artist
+        @album_img = album_img
+    end
+    
+    def get_lyrics
+        begin
+            fetcher = Lyricfy::Fetcher.new
+            lyrics = fetcher.search(@artist,@name)
+            @lyrics = lyrics.body.split('\n') # array of lyrics
+        rescue
+            @lyrics = ["Sorry, no lyrics found."]
+        end
+    end
+end
+
+class Playlist
+    
+    attr_reader :user, :playlist_id, :name, :songs
+    
+    def initialize(user,playlist_id)
+        @user = user
+        @playlist_id = playlist_id
+        @songs = [] # instances of the Song class
+    end
+    
+    def get_info
+        playlist = RSpotify::Playlist.find(@user,@playlist_id)
+        @name = playlist.name
+        
+        playlist.tracks.each do |track|
+            @songs << Song.new(track.name,track.artists.first.name,track.album.images.first["url"])
+        end
+    end
+    
+    def get_all_lyrics
+        @songs.each do |song|
+            song.get_lyrics
+        end
+    end
+end
